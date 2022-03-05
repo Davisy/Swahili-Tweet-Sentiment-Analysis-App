cf1a367672cad601ca977a198301ec125012ab1e
Davis David <davisdavid179@gmail.com>
2022-03-05 23:03:20 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import requests as r
+from os.path import dirname, join, realpath
+import joblib
+
+# add banner image
+st.header("Swahili Tweet Sentiment Analysis App")
+st.image("images/sentiment-image.png")
+st.subheader(
+    """
+A simple app to analyze the sentiment of Swahili tweets.
+"""
+)
+
+# form to collect news content
+my_form = st.form(key="news_form")
+tweet = my_form.text_input("Input your swahili tweet here")
+submit = my_form.form_submit_button(label="make prediction")
+
+
+# load the model and count_vectorizer
+
+with open(
+    join(dirname(realpath(__file__)), "model/swahili-sentiment-model.pkl"), "rb"
+) as f:
+    model = joblib.load(f)
+
+with open(join(dirname(realpath(__file__)), "preprocessing/vectorizer.pkl"), "rb") as f:
+    vectorizer = joblib.load(f)
+
+sentiments = {0: "Neutral", 1: "Positive", -1: "Negative"}
+
+
+if submit:
+
+    # transform the input
+    transformed_tweet = vectorizer.transform([tweet])
+
+    # perform prediction
+    prediction = model.predict(transformed_tweet)
+    output = int(prediction[0])
+    probas = model.predict_proba(transformed_tweet)
+    probability = "{:.2f}".format(float(probas[:, output]))
+
+    # Display results of the NLP task
+    st.header("Results")
+    if output == 1:
+        st.write("The sentiment of the tweet is {} 😊".format(sentiments[output]))
+    elif output == -1:
+        st.write("The sentiment of the tweet is {} 😡 ".format(sentiments[output]))
+    else:
+        st.write("The sentiment of the tweet is {} 😐".format(sentiments[output]))
+
+st.write("Developed with ❤️ by Davis David")
